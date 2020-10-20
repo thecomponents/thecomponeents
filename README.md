b86956ae784993c52250ce86ae6eab40ca4d23eb
Bartosz Dębicki <bdebicki@users.noreply.github.com>
2020-10-20 18:22:50 +0200

    basic configuration (#1)
    * extend .gitignore file
    * set basic folders structure
    * add webpack configuration
    * add initial scss
    * add .editorconfig
    * add stylelint configuration
    * add travic ci configuration
    * add husky configuration
    * update readme file

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# thecomponeents
+# thecomponents
+
+[![Build Status](https://travis-ci.org/thecomponents/thecomponents.svg?branch=master)](https://travis-ci.org/thecomponents/thecomponents)
+
+Open source library of components and helpers to help you build landings, dashboards and interfaces
+
+
+### commands
+`npm run dependencies` - install required dependencies  
+`npm run build` - build packages  
+`npm run lint:styles` - lint styles  
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const PostCSSPresetEnv = require('postcss-preset-env');
+const SvgToMiniDataURI = require('mini-svg-data-uri');
+
+const config = {
+  devtool: 'eval',
+  entry: './src/library.scss',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                PostCSSPresetEnv(),
+              ],
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.svg$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              generator: (content) => SvgToMiniDataURI(content.toString()),
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'library.css',
+    }),
+  ],
+};
+
+exports.default = config;
