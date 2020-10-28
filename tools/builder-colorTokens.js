9bc35f338c74962a63f79bdf87d1423789c27f1c
bdebicki <bartosz.debicki@zoin.pl>
2020-10-28 17:01:13 +0100

    read from yamn by js-yaml and build scss value string

--- tools/builder-colorTokens.js
+++ tools/builder-colorTokens.js
@@ -1,13 +1,33 @@
 const fs = require('fs');
 const path = require('path');
-const getTokensFromYmlFile = require('./utils/getTokensFromYmlFile');
+const yaml = require('js-yaml');
 
+let tokens = {};
 const colorTokensFile = path.join(__dirname, '../src/tokens/colors.yml');
-const colorTokens = getTokensFromYmlFile(colorTokensFile);
-colorTokens.toString();
-JSON.parse(colorTokens);
-// const colorSassTokens = colorTokens.replaceAll("/([#A-Za-z0-9])\w+/g", "\"$1\"")
 
+try {
+  let fileContents = fs.readFileSync(colorTokensFile, 'utf8');
+  tokens = yaml.safeLoad(fileContents);
+} catch (e) {
+  console.log(e)
+}
 
-console.log(colorTokens);
+function boczek() {
+  const scssTokensString = JSON.stringify(tokens);
+  const scssTokensStringSplice = scssTokensString.slice(1, -1);
+  const scssTokensStringSpliceScssObject = scssTokensStringSplice
+    .replace("{", "(")
+    .replace("}", ")")
+    .replace(/,/g, ",\n")
+    .replace(":(\"", ": (\n\"")
+    .replace("\")", "\"\n)")
+    .replace(/":"/g, "\": \"")
+    .replace(/"[a-z]\w+": [(]/g, (v) => `$${v.replace(/"/g, '')}`)
+    .replace(/"[a-z0-9]\w+": /g, (v) => `\t${v}`);
+
+  return scssTokensStringSpliceScssObject;
+}
+
+console.log(boczek())
+
 console.log('text color classes has been generated');
