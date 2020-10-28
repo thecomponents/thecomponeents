eebdf94f33306fafaf6fac5cc71cb19043c9b6f6
bdebicki <bartosz.debicki@zoin.pl>
2020-10-28 20:53:55 +0100

    clean up color tokens builder

--- tools/builder-colorTokens.js
+++ tools/builder-colorTokens.js
@@ -4,6 +4,7 @@ const yaml = require('js-yaml');
 
 let tokens = {};
 const colorTokensFile = path.join(__dirname, '../src/tokens/colors.yml');
+const tokensScssFile = path.join(__dirname, '../src/tokens/colors-generated.scss');
 
 try {
   let fileContents = fs.readFileSync(colorTokensFile, 'utf8');
@@ -12,22 +13,24 @@ try {
   console.log(e)
 }
 
-function boczek() {
+function convertYmlToScss() {
   const scssTokensString = JSON.stringify(tokens);
   const scssTokensStringSplice = scssTokensString.slice(1, -1);
   const scssTokensStringSpliceScssObject = scssTokensStringSplice
-    .replace("{", "(")
-    .replace("}", ")")
-    .replace(/,/g, ",\n")
-    .replace(":(\"", ": (\n\"")
-    .replace("\")", "\"\n)")
-    .replace(/":"/g, "\": \"")
-    .replace(/"[a-z]\w+": [(]/g, (v) => `$${v.replace(/"/g, '')}`)
-    .replace(/"[a-z0-9]\w+": /g, (v) => `\t${v}`);
+    .replace("{", "(") // replace { by (
+    .replace("}", ");\n") // replace } by ); and new line
+    .replace(/,/g, ",\n") // set each token in new line
+    .replace(":(\"", ": (\n\"") // break line after variable's ( and space before it (and after :)
+    .replace("\")", "\"\n)") // break line before closing )
+    .replace(/":"/g, "\": \"") // add space after token name's :
+    .replace(/"[a-z]\w+": [(]/g, (s) => `$${s.replace(/"/g, '')}`) // add $ in the beginning of variable name
+    .replace(/"[a-z0-9]\w+": /g, (s) => `\t${s}`); // add tabs in indent
 
   return scssTokensStringSpliceScssObject;
 }
 
-console.log(boczek())
+console.log(convertYmlToScss());
+
+fs.writeFileSync(tokensScssFile, convertYmlToScss(), 'utf-8');
 
 console.log('text color classes has been generated');
