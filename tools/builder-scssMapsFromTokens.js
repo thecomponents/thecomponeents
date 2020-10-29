cd45e716b7f537309b1cca33d6c0fdbce2186814
bdebicki <bartosz.debicki@zoin.pl>
2020-10-29 20:17:41 +0100

    change maps script to accepts multiple files + add logs & debug

--- tools/builder-scssMapsFromTokens.js
+++ tools/builder-scssMapsFromTokens.js
@@ -2,35 +2,56 @@ const fs = require('fs');
 const path = require('path');
 const yaml = require('js-yaml');
 
-let tokens = {};
-const colorTokensFile = path.join(__dirname, '../src/tokens/colors.yml');
-const tokensScssFile = path.join(__dirname, '../src/maps/colors.scss');
+const DEBUG_FLAG = '--debug';
 
-try {
-  let fileContents = fs.readFileSync(colorTokensFile, 'utf8');
-  tokens = yaml.safeLoad(fileContents);
-} catch (e) {
-  console.log(e)
+let debug = false;
+const args = process.argv;
+const categories = args.slice(2);
+
+if (args.includes(DEBUG_FLAG)) {
+  debug = true;
+  const debugIndex = categories.indexOf(DEBUG_FLAG);
+  categories.splice(debugIndex, 1);
 }
 
-function convertYmlToScss() {
-  const scssTokensString = JSON.stringify(tokens);
-  const scssTokensStringSplice = scssTokensString.slice(1, -1);
-  const scssTokensStringSpliceScssObject = scssTokensStringSplice
-    .replace("{", "(") // replace { by (
-    .replace("}", ");\n") // replace } by ); and new line
-    .replace(/,/g, ",\n") // set each token in new line
-    .replace(":(\"", ": (\n\"") // break line after variable's ( and space before it (and after :)
-    .replace("\")", "\"\n)") // break line before closing )
-    .replace(/":"/g, "\": \"") // add space after token name's :
-    .replace(/"[a-z]\w+": [(]/g, (s) => `$${s.replace(/"/g, '')}`) // add $ in the beginning of variable name
-    .replace(/"[a-z0-9]\w+": /g, (s) => `\t${s}`); // add tabs in indent
+console.log('start generating scss maps');
 
-  return scssTokensStringSpliceScssObject;
-}
+categories.forEach((category) => {
+  let tokens = {};
+  const srcFile = path.join(__dirname, `../src/tokens/${category}.yml`);
+  const distFile = path.join(__dirname, `../src/maps/${category}.scss`);
 
-console.log(convertYmlToScss());
+  try {
+    let fileContents = fs.readFileSync(srcFile, 'utf8');
+    tokens = yaml.safeLoad(fileContents);
+  } catch (error) {
+    console.log(`— ${category} map could not be generated because of error${!debug ? `; run 'build:scssMaps:debug' for details` : ''}`);
+    if (debug) {
+      console.log(error);
+      console.log(' ');
+    }
+    return false;
+  }
 
-fs.writeFileSync(tokensScssFile, convertYmlToScss(), 'utf-8');
+  function convertTokensToMap() {
+    return JSON.stringify(tokens)
+      .slice(1, -1) // remove { and } over tokens
+      .replace("{", "(") // replace { by (
+      .replace("}", ");\n") // replace } by ); and new line
+      .replace(/,/g, ",\n") // set each token in new line
+      .replace(":(\"", ": (\n\"") // break line after variable's ( and space before it (and after :)
+      .replace("\")", "\"\n)") // break line before closing )
+      .replace(/":"/g, "\": \"") // add space after token name's :
+      .replace(/"[a-z]\w+": [(]/g, (s) => `$${s.replace(/"/g, '')}`) // add $ in the beginning of variable name
+      .replace(/"[a-z0-9]\w+": /g, (s) => `\t${s}`); // add tabs in indent
+  }
 
-console.log('text color classes has been generated');
+  console.log(`| ${category} map has been created${debug ? ':' : ''}`);
+  if (debug) {
+    console.log(`  ${convertTokensToMap()}`);
+  }
+
+  fs.writeFileSync(distFile, convertTokensToMap(), 'utf-8');
+});
+
+console.log('finish generating maps');
