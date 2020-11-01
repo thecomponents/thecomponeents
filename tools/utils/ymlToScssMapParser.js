9f9395c66982260fc85bf06a1caf5bd2a68dbfa0
bdebicki <bartosz.debicki@zoin.pl>
2020-11-01 20:47:33 +0100

    build weights tokens from yml

--- tools/utils/ymlToScssMapParser.js
+++ tools/utils/ymlToScssMapParser.js
@@ -5,8 +5,8 @@ function convertTokensToMap(data) {
     .replace("}", ");\n") // replace } by ); and new line
     .replace(/,/g, ",\n") // set each token in new line
     .replace(":(\"", ": (\n\"") // break line after variable's ( and space before it (and after :)
-    .replace("\")", "\"\n)") // break line before closing )
-    .replace(/":"/g, "\": \"") // add space after token name's :
+    .replace(/"|[0-9][)]/g, (s) => `${s.replace(")","\n)")}`) // break line before closing )
+    .replace(/":["|[0-9]/g, (s) => `${s.replace("\":", "\": ")}`) // add space after token name's :
     .replace(/"[a-z]\w+": [(]/g, (s) => `$${s.replace(/"/g, '')}`) // add $ in the beginning of variable name
     .replace(/"[a-z0-9]\w*": /g, (s) => `\t${s}`); // add tabs in indent
 }
--- tools/utils/ymlToScssTypographyParser.js
+++ tools/utils/ymlToScssTypographyParser.js
@@ -1,7 +1,10 @@
+const ymlToScssMapParser = require("./ymlToScssMapParser");
+
 function ymlToScssTypographyParser(data) {
   let typography = [];
   let parsedTypography;
   let imports = '';
+  let weights = '';
 
   Object.keys(data).forEach((typographyCategory) => {
     if (typographyCategory === 'import') {
@@ -10,6 +13,13 @@ function ymlToScssTypographyParser(data) {
 
         imports = imports += currentImport;
       });
+    } else if (typographyCategory === 'weight') {
+      const dataWithKey = {
+        weight: {
+          ...data[typographyCategory]
+        }
+      }
+      weights = ymlToScssMapParser(dataWithKey);
     } else {
       let keyCategory = typographyCategory;
       let keyScale;
@@ -33,7 +43,7 @@ function ymlToScssTypographyParser(data) {
     .replace(/","/g, ",\n")
     .replace(/\("/g, `\t("`);
 
-  return `${imports}\n${parsedTypography}`;
+  return `${imports}\n${parsedTypography}\n${weights}`;
 }
 
 module.exports = ymlToScssTypographyParser;
